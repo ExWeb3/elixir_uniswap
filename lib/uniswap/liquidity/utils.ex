c049f6c172ee1c028fb88e32c871ae736ee4dade
Alisina Bahadori <alisina.bm@gmail.com>
2022-09-18 00:10:52 +0200

    Add docs and typespecs to Liquidity.Utils

--- lib/uniswap/liquidity/utils.ex
+++ lib/uniswap/liquidity/utils.ex
@@ -2,9 +2,9 @@ defmodule Uniswap.Liquidity.Utils do
   @moduledoc """
   Utilities for handling liquidity calculations
 
-  Provides functions for computing liquidity amounts from token amounts and prices
+  Provides functions for computing liquidity amounts from token amounts and prices.
 
-  Note that all prices in this module are presented as `token0/token1`.
+  Note that all prices in this module are presented as `token0 / token1`.
   """
 
   @doc """
@@ -16,14 +16,16 @@ defmodule Uniswap.Liquidity.Utils do
   - amount_0: The amount of token0 being sent in.
   - price_a: A price representing the first tick boundary.
   - price_b: A price representing the second tick boundary.
-  - decimals_0: Decimal precision of token0. (= ERC20.decimals)
-  - decimals_1: Decimal precision of token1. (= ERC20.decimals)
+  - decimals_0: Decimal precision of token0. (= ERC20.decimals) Defaults to 18.
+  - decimals_1: Decimal precision of token1. (= ERC20.decimals) Defaults to 18.
 
   ## Examples
 
   iex> Uniswap.Liquidity.Utils.get_liquidity_for_amount_0(100, 110/100, 100/110)
   1048
   """
+  @spec get_liquidity_for_amount_0(number, number, number, non_neg_integer, non_neg_integer) ::
+          non_neg_integer
   def get_liquidity_for_amount_0(
         amount_0,
         price_a,
@@ -50,14 +52,16 @@ defmodule Uniswap.Liquidity.Utils do
   - amount_1: The amount of token1 being sent in.
   - price_a: A price representing the first tick boundary.
   - price_b: A price representing the second tick boundary.
-  - decimals_0: Decimal precision of token0. (= ERC20.decimals)
-  - decimals_1: Decimal precision of token1. (= ERC20.decimals)
+  - decimals_0: Decimal precision of token0. (= ERC20.decimals) Defaults to 18.
+  - decimals_1: Decimal precision of token1. (= ERC20.decimals) Defaults to 18.
 
   ## Examples
 
   iex> Uniswap.Liquidity.Utils.get_liquidity_for_amount_1(100, 110/100, 100/110)
   1048
   """
+  @spec get_liquidity_for_amount_1(number, number, number, non_neg_integer, non_neg_integer) ::
+          non_neg_integer
   def get_liquidity_for_amount_1(
         amount_1,
         price_a,
@@ -78,6 +82,7 @@ defmodule Uniswap.Liquidity.Utils do
   Computes the maximum amount of liquidity received for a given amount of token0, token1, the current
   pool prices and the prices at the tick boundaries.
 
+  Returns the largest possible liquidity for the amounts.
 
   ## Parameters
   - amount_0: The amount of token0 being sent in.
@@ -85,15 +90,10 @@ defmodule Uniswap.Liquidity.Utils do
   - current_price: A price representing the current pool price.
   - price_a: A price representing the first tick boundary.
   - price_b: A price representing the second tick boundary.
-  - decimals_0: Decimal precision of token0. (= ERC20.decimals)
-  - decimals_1: Decimal precision of token1. (= ERC20.decimals)
+  - decimals_0: Decimal precision of token0. (= ERC20.decimals) Defaults to 18.
+  - decimals_1: Decimal precision of token1. (= ERC20.decimals) Defaults to 18.
 
   ## Examples
-
-  iex> Uniswap.Liquidity.Utils.get_liquidity_for_amounts(
-  ...> 1244.2721282001135, 0, 1/2000, 1/1474.4463585092642, 1/1813.5245551669193, 6, 18
-  ...> )
-  297176747167880
 
   iex> Uniswap.Liquidity.Utils.get_liquidity_for_amounts(100, 200, 1, 100/110, 110/100)
   2148
@@ -104,6 +104,15 @@ defmodule Uniswap.Liquidity.Utils do
   iex> Uniswap.Liquidity.Utils.get_liquidity_for_amounts(100, 200, 111/100, 100/110, 110/100)
   2097
   """
+  @spec get_liquidity_for_amounts(
+          number,
+          number,
+          number,
+          number,
+          number,
+          non_neg_integer,
+          non_neg_integer
+        ) :: non_neg_integer
   def get_liquidity_for_amounts(
         amount_0,
         amount_1,
@@ -123,10 +132,10 @@ defmodule Uniswap.Liquidity.Utils do
         do_get_liquidity_for_amount_0(amount_0, s_lower_price, s_upper_price)
 
       s_current_price < s_upper_price ->
-        liquidity_for_0 = do_get_liquidity_for_amount_0(amount_0, s_current_price, s_upper_price)
-        liquidity_for_1 = do_get_liquidity_for_amount_1(amount_1, s_lower_price, s_current_price)
+        liquidity_0 = do_get_liquidity_for_amount_0(amount_0, s_current_price, s_upper_price)
+        liquidity_1 = do_get_liquidity_for_amount_1(amount_1, s_lower_price, s_current_price)
 
-        min(liquidity_for_0, liquidity_for_1)
+        min(liquidity_0, liquidity_1)
 
       true ->
         # s_current_price >= s_upper_price
@@ -136,7 +145,23 @@ defmodule Uniswap.Liquidity.Utils do
     |> trunc()
   end
 
-  def get_amount_0_for_liquidity(liquidity, price_a, price_b, decimals_0, decimals_1) do
+  @doc """
+  Computes the amount of token0 for a given amount of liquidity and a price range
+
+  ## Parameters
+  - liquidity: The liquidity being valued.
+  - price_a: A price representing the first tick boundary.
+  - price_b: A price representing the second tick boundary.
+  - decimals_0: Decimal precision of token0. (= ERC20.decimals) Defaults to 18.
+  - decimals_1: Decimal precision of token1. (= ERC20.decimals) Defaults to 18.
+
+  ## Examples
+  iex> Uniswap.Liquidity.Utils.get_amount_0_for_liquidity(1048, 100 / 110, 110 / 100)
+  99.9228793529381
+  """
+  @spec get_amount_0_for_liquidity(number, number, number, non_neg_integer, non_neg_integer) ::
+          float
+  def get_amount_0_for_liquidity(liquidity, price_a, price_b, decimals_0 \\ 18, decimals_1 \\ 18) do
     {lower_price, upper_price} = sort_prices(price_a, price_b)
     s_lower_price = sqrtx96(lower_price)
     s_upper_price = sqrtx96(upper_price)
@@ -144,7 +169,23 @@ defmodule Uniswap.Liquidity.Utils do
     do_get_amount_0_for_liquidity(liquidity, s_lower_price, s_upper_price, decimals_0, decimals_1)
   end
 
-  def get_amount_1_for_liquidity(liquidity, price_a, price_b, decimals_0, decimals_1) do
+  @doc """
+  Computes the amount of token1 for a given amount of liquidity and a price range
+
+  ## Parameters
+  - liquidity: The liquidity being valued.
+  - price_a: A price representing the first tick boundary.
+  - price_b: A price representing the second tick boundary.
+  - decimals_0: Decimal precision of token0. (= ERC20.decimals) Defaults to 18.
+  - decimals_1: Decimal precision of token1. (= ERC20.decimals) Defaults to 18.
+
+  ## Examples
+  iex> Uniswap.Liquidity.Utils.get_amount_0_for_liquidity(2097, 100 / 110, 110 / 100)
+  199.94110496480076
+  """
+  @spec get_amount_1_for_liquidity(number, number, number, non_neg_integer, non_neg_integer) ::
+          float
+  def get_amount_1_for_liquidity(liquidity, price_a, price_b, decimals_0 \\ 18, decimals_1 \\ 18) do
     {lower_price, upper_price} = sort_prices(price_a, price_b)
     s_lower_price = sqrtx96(lower_price)
     s_upper_price = sqrtx96(upper_price)
@@ -152,6 +193,38 @@ defmodule Uniswap.Liquidity.Utils do
     do_get_amount_1_for_liquidity(liquidity, s_lower_price, s_upper_price, decimals_0, decimals_1)
   end
 
+  @doc """
+  Computes the token0 and token1 value for a given amount of liquidity, the current
+  pool prices and the prices at the tick boundaries.
+
+  Returns amounts in a 2 element tuple: `{amount_token_0, amount_token_1}`
+
+  ## Parameters
+  - liquidity: The liquidity being valued
+  - current_price: A price representing the current pool price.
+  - price_a: A price representing the first tick boundary.
+  - price_b: A price representing the second tick boundary.
+  - decimals_0: Decimal precision of token0. (= ERC20.decimals) Defaults to 18.
+  - decimals_1: Decimal precision of token1. (= ERC20.decimals) Defaults to 18.
+
+  ## Examples
+  iex> Uniswap.Liquidity.Utils.get_amounts_for_liquidity(2148, 1, 100/110, 110/100)
+  {99.96235830046787, 99.96235830046763}
+
+  iex> Uniswap.Liquidity.Utils.get_amounts_for_liquidity(1048, 99/110, 100/110, 110/100)
+  {99.9228793529381, 0}
+
+  iex> Uniswap.Liquidity.Utils.get_amounts_for_liquidity(2097, 111/100, 100/110, 110/100)
+  {0, 199.94110496480081}
+  """
+  @spec get_amounts_for_liquidity(
+          number,
+          number,
+          number,
+          number,
+          non_neg_integer,
+          non_neg_integer
+        ) :: {float, float}
   def get_amounts_for_liquidity(
         liquidity,
         current_price,
@@ -178,15 +251,15 @@ defmodule Uniswap.Liquidity.Utils do
       s_current_price < s_upper_price ->
         {do_get_amount_0_for_liquidity(
            liquidity,
-           s_lower_price,
            s_current_price,
+           s_upper_price,
            decimals_0,
            decimals_1
          ),
          do_get_amount_1_for_liquidity(
            liquidity,
+           s_lower_price,
            s_current_price,
-           s_upper_price,
            decimals_0,
            decimals_1
          )}
@@ -203,6 +276,8 @@ defmodule Uniswap.Liquidity.Utils do
          )}
     end
   end
+
+  ## Helpers
 
   defp do_get_amount_0_for_liquidity(
          liquidity,
--- test/uniswap/liquidity/utils_test.exs
+++ test/uniswap/liquidity/utils_test.exs
@@ -1,4 +1,30 @@
 defmodule Uniswap.Liquidity.UtilsTest do
   use ExUnit.Case
   doctest Uniswap.Liquidity.Utils
+
+  alias Uniswap.Liquidity.Utils
+
+  describe "get_liquidity_for_amounts/7" do
+    test "with on-chain data" do
+      amount_0 = 1244.2721282001135
+      amount_1 = 0
+      price = 1 / 2000
+      price_a = 1 / 1474.4463585092642
+      price_b = 1 / 1813.5245551669193
+      decimals_0 = 6
+      decimals_1 = 18
+      liquidity = 297_176_747_167_880
+
+      assert liquidity ==
+               Utils.get_liquidity_for_amounts(
+                 amount_0,
+                 amount_1,
+                 price,
+                 price_a,
+                 price_b,
+                 decimals_0,
+                 decimals_1
+               )
+    end
+  end
 end
