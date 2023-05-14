1d49cbb23749b4b8be174b9347d3df45bfb09289
Alisina Bahadori <alisina.bm@gmail.com>
2023-05-13 23:44:36 -0400

    Add factory default address

--- lib/uniswap/contracts/v3_factory.ex
+++ lib/uniswap/contracts/v3_factory.ex
@@ -7,6 +7,7 @@ defmodule Uniswap.Contracts.V3Factory do
               :code.priv_dir(:uniswap),
               "node_modules/@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json"
             )
+  @default_address "0x1F98431c8aD98523631AE4a59f267346ea31F984"
 
-  use Ethers.Contract, abi_file: @abi_file
+  use Ethers.Contract, abi_file: @abi_file, default_address: @default_address
 end
