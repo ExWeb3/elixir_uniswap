ad490fc563f3ffe6c2609b26c31f376bd7167c58
Alisina Bahadori <alisina.bm@gmail.com>
2024-02-13 17:50:44 -0500

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -58,6 +58,17 @@ defmodule Uniswap.MixProject do
       extras: [
         "README.md": [title: "Introduction"]
       ],
+      groups_for_modules: [
+        "Math Helpers": [
+          ~r/^Uniswap\..*\.Math$/
+        ],
+        "Uniswap Contracts": [
+          ~r/^Uniswap\.Contracts\.(?:(?!EventFilters$).)*$/
+        ],
+        "Event Filters": [
+          ~r/^Uniswap\.Contracts\..*\.EventFilters$/
+        ]
+      ],
       source_url: @source_url,
       source_ref: source_ref
     ]
@@ -74,6 +85,7 @@ defmodule Uniswap.MixProject do
       {:decimal, "~> 2.1", only: :test},
       {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
       {:ethers, "~> 0.3.0 or ~> 0.2.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test}
     ]
   end
