9c869621414e63f4ff67a660493ae987c527f3f6
Alisina Bahadori <alisina.bm@gmail.com>
2025-04-13 12:36:34 -0400

    Fix SwapRouter default address

--- lib/uniswap/contracts/swap_router.ex
+++ lib/uniswap/contracts/swap_router.ex
@@ -5,7 +5,7 @@ defmodule Uniswap.Contracts.SwapRouter do
   import Uniswap.Contracts.Utils
 
   @abi_file abi_path("v3-periphery", "SwapRouter.sol/SwapRouter.json")
-  @default_address "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45"
+  @default_address "0xE592427A0AEce92De3Edee1F18E0157C05861564"
 
   use Ethers.Contract, abi_file: @abi_file, default_address: @default_address
 end
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Uniswap.MixProject do
   use Mix.Project
 
-  @version "0.0.3"
+  @version "0.0.4"
   @source_url "https://github.com/alisinabh/elixir_uniswap"
 
   def project do
